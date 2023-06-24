60102a02fa5c7a9d68832f90ec765990581fcc0c
lambang <sasangkakurniawan.com>
2023-06-24 13:50:11 +0700

    add submit

--- src/components/TaskProgress/TaskColumn.tsx
+++ src/components/TaskProgress/TaskColumn.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TaskCard } from './TaskCard';
 import type { Task, CSSProperties } from '../../types';
 import TaskModal from '../shared/TaskModal'; // Ditambahkan
-import { TASK_PROGRESS_ID } from '../../constants/app'; // Ditambahkan
+import { TASK_MODAL_TYPE, TASK_PROGRESS_ID } from '../../constants/app'; // Ditambahkan
 import { useState } from 'react'; // useState ditambahkan
 interface TaskColumnProps {
     columnTitle: string;
@@ -26,12 +26,12 @@ const TaskColumn = ({ columnTitle, tasks }: TaskColumnProps): JSX.Element => {
                 })}
             </div>
             {isModalOpen && (
-                <TaskModal
-                    headingTitle='Add your task'
-                    setIsModalOpen={setIsModalOpen}
-                    defaultProgressOrder={TASK_PROGRESS_ID.NOT_STARTED}
-                    type='add'
-                />
+                   <TaskModal
+                   headingTitle="Add your task"
+                   type={TASK_MODAL_TYPE.ADD} // Ditambahkan
+                   setIsModalOpen={setIsModalOpen}
+                   defaultProgressOrder={TASK_PROGRESS_ID.NOT_STARTED}
+                 />
             )}
         </div>
     );
@@ -40,6 +40,11 @@ const TaskColumn = ({ columnTitle, tasks }: TaskColumnProps): JSX.Element => {
 const styles: CSSProperties = {
     plusIcon: {
         cursor: 'pointer',
+        color: '#55C89F',
+        backgroundColor: 'red',
+        padding: '2px',
+        borderRadius: '4px',
+        marginRight: '4px',
     },
     categoryColumn: {
         width: '22%',
@@ -50,12 +55,24 @@ const styles: CSSProperties = {
         alignItems: 'center',
         padding: '0 4px',
     },
+    button: {
+        cursor: 'pointer',
+        backgroundColor: '#55C89F',
+        color: '#fff',
+        padding: '4px 8px',
+        borderRadius: '4px',
+    },
+    categoryTitle: {
+        fontWeight: 'bold',
+    }
 };
 
 // Responsive styles
 styles.categoryColumn = {
     ...styles.categoryColumn,
     width: '20%',
+    display: 'flex',
+    flexDirection: 'column',
 };
 
 export default TaskColumn;
