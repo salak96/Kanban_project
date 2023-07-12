10462a9d8ea023ae221b16fe25cede693fc7e7df
lambang <sasangkakurniawan.com>
2023-07-12 20:56:05 +0700

    npx jest DummyTest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default { // Diperbarui ke sintaks untuk module ES
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
---
+++ src/__tests__/DummyTest.test.tsx
@@ -0,0 +1,5 @@
+describe('Dummy test', () => {
+  test('Check Jest is installed', () => {
+    console.log('OK')
+  })
+})
