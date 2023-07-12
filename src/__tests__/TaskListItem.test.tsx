56f02ae030e90b9aa7460abb509c5b9b1b7a93a4
lambang <sasangkakurniawan.com>
2023-07-13 00:54:35 +0700

    add dan edit error

--- src/__tests__/TaskListItem.test.tsx
+++ src/__tests__/TaskListItem.test.tsx
@@ -58,3 +58,66 @@ describe('Delete Task Feature', () => {
     })
   })
 })
+describe('Add Task Feature', () => {
+    test('A new task can be added', async () => {
+      let lengthBefore = 0
+      await waitFor(() => {
+        user.click(screen.getByTestId('add-task-button'))
+        lengthBefore = screen.getAllByTestId('task-list-item').length
+        expect(screen.getByTestId('task-modal')).toBeInTheDocument()
+      })
+  
+      // Masukkan judul
+      const inputTitle = 'Task baru 1'
+      await user.type(screen.getByTestId('title-input'), inputTitle)
+      expect(screen.getByTestId('title-input')).toHaveValue(inputTitle)
+  
+      // Masukkan detail
+      const inputDetail = 'Detail Task baru 1'
+      await user.type(screen.getByTestId('detail-input'), inputDetail)
+      expect(screen.getByTestId('detail-input')).toHaveValue(inputDetail)
+  
+      // Pilih tanggal
+      const inputDate = '2022-09-09'
+      await user.clear(screen.getByTestId('due-date-input'))
+      await user.type(screen.getByTestId('due-date-input'), inputDate)
+      expect(screen.getByTestId('due-date-input')).toHaveValue(inputDate)
+  
+      // Pilih progress
+      const inputProgress = '3'
+      await user.selectOptions(screen.getByTestId('progress-select'), inputProgress)
+      expect(screen.getByTestId('progress-select')).toHaveValue(inputProgress)
+  
+      // Klik submit
+      await user.click(screen.getByTestId('task-modal-submit-button'))
+  
+      await waitFor(() => {
+        const lengthAfter = screen.getAllByTestId('task-list-item').length
+        expect(lengthBefore).toBeLessThan(lengthAfter)
+      })
+    })
+  })
+  describe('Edit Task Feature', () => {
+    test('A task can be edited', async () => {
+      const newInputTitle = 'Task baru nya'
+  
+      await waitFor(() => {
+        user.click(screen.getAllByTestId('task-menu-button')[0])
+        expect(screen.getByTestId('task-menu')).toBeInTheDocument()
+      })
+  
+      await user.click(screen.getByTestId('edit-button'))
+  
+      await waitFor(() => {
+        expect(screen.getByTestId('task-modal')).toBeInTheDocument()
+      })
+  
+      await user.clear(screen.getByTestId('title-input'))
+      await user.type(screen.getByTestId('title-input'), newInputTitle)
+      await user.click(screen.getByTestId('task-modal-submit-button'))
+  
+      await waitFor(() => {
+        expect(screen.getByText(newInputTitle)).toBeInTheDocument()
+      })
+    })
+  })
